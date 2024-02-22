95ada04747797eca1a68a7b13409eca4c7760c2f
m-ehab2 <muhamedehab1@gmail.com>
2024-02-23 01:57:20 +0200

    rewrote the schema to match the new voucher model

--- validation/voucherCreationSchema.js
+++ validation/voucherCreationSchema.js
@@ -4,6 +4,7 @@ const Joi = require("joi");
 const voucherValidationSchema = Joi.object({
   code: Joi.string().required(),
   discount: Joi.number().required().min(0),
+  type: Joi.string().required().valid("flat", "percentage"),
   expiryDate: Joi.date().iso().required(),
   maxUsage: Joi.number().integer().min(1).allow(null),
   currentUsage: Joi.number().integer().min(0).default(0),
