3a9efbe0b4c9f7a5617a45fae5e4e9d7d187c509
m-ehab2 <muhamedehab1@gmail.com>
2024-02-26 11:33:01 +0200

    changed the username to be unique

--- models/admin.js
+++ models/admin.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 // Define admin schema
 const adminSchema = new mongoose.Schema(
   {
-    username: { type: String, required: true },
+    username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     createdBy: { type: String },
     authorities: {
