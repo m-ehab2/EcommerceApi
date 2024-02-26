47d381e9956d510e8cfb55f0c0947f0577b07dca
m-ehab2 <muhamedehab1@gmail.com>
2024-02-26 11:37:09 +0200

    rewrote the validation to account for the new model

--- validation/ticketCreationValidation.js
+++ validation/ticketCreationValidation.js
@@ -1,10 +1,12 @@
 const Joi = require("joi");
 
-// Define the ticket creation schema for validation
+// Define Joi schema for ticket creation
 const ticketCreationSchema = Joi.object({
   title: Joi.string().required(),
   description: Joi.string().required(),
-  product_ids: Joi.array().items(Joi.string()).required(),
+  item_ids: Joi.array()
+    .items(Joi.string().pattern(/^[0-9a-fA-F]{24}$/))
+    .required(),
 });
 
 module.exports = ticketCreationSchema;
